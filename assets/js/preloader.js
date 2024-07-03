a4ee6e3623a74ae7bb07776f34179127e20c456c
Traniks <andert2017@gmail.com>
2024-07-03 23:26:59 +0300

    feat: fix social links + fix zoom on macbock + add preloader

---
+++ assets/js/preloader.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function () {
+	const preloader = document.getElementById('preloader')
+	const percentsElement = document.getElementById('percents')
+	const content = document.getElementById('content')
+
+	let images = document.images
+	let imagesTotalCount = images.length
+	let imagesLoadedCount = 0
+
+	for (let i = 0; i < imagesTotalCount; i++) {
+		let img = new Image()
+		img.onload = imageLoaded
+		img.onerror = imageLoaded
+		img.src = images[i].src
+	}
+
+	function imageLoaded() {
+		imagesLoadedCount++
+		let percents = ((100 / imagesTotalCount) * imagesLoadedCount) << 0
+		percentsElement.textContent = percents
+
+		if (imagesLoadedCount >= imagesTotalCount) {
+			setTimeout(function () {
+				preloader.style.display = 'none'
+				content.style.display = 'block'
+			}, 500) // Add a small delay if needed
+		}
+	}
+})
